915c5528fe6a8b0944fb27ce255d3c0c0864f452
3sdd <56486103+3sdd@users.noreply.github.com>
2021-02-06 19:48:19 +0900

    voc_collate_fnのコメントアウト削除

--- utils/voc_collate_fn.py
+++ utils/voc_collate_fn.py
@@ -6,23 +6,10 @@ def voc_collate_fn(batch_list):
     annotations = []
     for i in range(len(batch_list)):
         annotations.append(batch_list[i][1]["annotation"])
-    # for k in batch_list[0][1]['annotation']:
-    #     annotations[k] = [batch_list[i][1]['annotation'][k] for i in range(len(batch_list))]
 
     for i in range(len(annotations)):
         obj=annotations[i]["object"]
         if not isinstance(obj,list):
             annotations[i]["object"]=[obj]
 
-        # for obj in annotations[i]["object"]:
-        #     # print(obj)
-        #     # object_list = []
-
-        #     if not isinstance(obj,list):
-        #         # obj=[obj]
-        #         # object_list.append(l)
-
-        #         annotations[i]["object"]=[obj]
-
-    # annotations['object'] = object_list
     return {'images':images,'annotations':annotations}
