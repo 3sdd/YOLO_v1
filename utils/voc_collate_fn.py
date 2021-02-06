c46140f5408f4f0d94e3bf97c907d48e1432bf5f
3sdd <56486103+3sdd@users.noreply.github.com>
2021-02-06 21:00:57 +0900

    voc_collate_fn内のannotationをリスト内包表記で作成に変更してみた

--- utils/voc_collate_fn.py
+++ utils/voc_collate_fn.py
@@ -3,8 +3,6 @@ from torch.utils.data.dataloader import default_collate
 def voc_collate_fn(batch_list):
     images = default_collate([batch_list[i][0] for i in range(len(batch_list))])
     
-    annotations = []
-    for i in range(len(batch_list)):
-        annotations.append(batch_list[i][1]["annotation"])
+    annotations = [item[1]["annotation"] for item in batch_list]
 
     return {'images':images,'annotations':annotations}
